00782aff35e07bf4db4d1e1f855c323e33c652a1
Robert Tuck <robtuck@gmail.com>
2017-11-19 22:55:09 -0500

    Well, it's a solution

--- Programming-Assignment-1/network_packet_processing_simulation/process_packages.py
+++ Programming-Assignment-1/network_packet_processing_simulation/process_packages.py
@@ -1,14 +1,19 @@
 # python3
 
+# from collections import deque
+
+
 class Request:
     def __init__(self, arrival_time, process_time):
         self.arrival_time = arrival_time
         self.process_time = process_time
 
+
 class Response:
     def __init__(self, dropped, start_time):
         self.dropped = dropped
         self.start_time = start_time
+
 
 class Buffer:
     def __init__(self, size):
@@ -17,7 +22,23 @@ class Buffer:
 
     def Process(self, request):
         # write your code here
-        return Response(False, -1)
+        while self.finish_time_:
+            if self.finish_time_[0] <= request.arrival_time:
+                self.finish_time_.pop(0)
+            else:
+                break
+
+        if len(self.finish_time_) == self.size:
+            return Response(True, -1)
+
+        if len(self.finish_time_) == 0:
+            self.finish_time_ = [request.arrival_time + request.process_time]
+            return Response(False, request.arrival_time)
+
+        response = Response(False, self.finish_time_[-1])
+        self.finish_time_.append(self.finish_time_[-1] + request.process_time)
+        return response
+
 
 def ReadRequests(count):
     requests = []
@@ -26,15 +47,18 @@ def ReadRequests(count):
         requests.append(Request(arrival_time, process_time))
     return requests
 
+
 def ProcessRequests(requests, buffer):
     responses = []
     for request in requests:
         responses.append(buffer.Process(request))
     return responses
 
+
 def PrintResponses(responses):
     for response in responses:
         print(response.start_time if not response.dropped else -1)
+
 
 if __name__ == "__main__":
     size, count = map(int, input().strip().split())
--- Programming-Assignment-1/tree_height/tree-height.py
+++ Programming-Assignment-1/tree_height/tree-height.py
@@ -1,15 +1,24 @@
 # python3
 
-import sys, threading
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**27)  # new thread will get stack of such size
+import sys
+import threading
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
 
 class TreeHeight:
         def read(self):
                 self.n = int(sys.stdin.readline())
                 self.parent = list(map(int, sys.stdin.readline().split()))
 
-        def compute_height(self):
+        def compute_height_naive(self):
                 # Replace this code with a faster implementation
                 maxHeight = 0
                 for vertex in range(self.n):
@@ -18,12 +27,58 @@ class TreeHeight:
                         while i != -1:
                                 height += 1
                                 i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+                        maxHeight = max(maxHeight, height)
+                return maxHeight
+
+
+def fillDepth(parent, i, depth):
+
+    # If depth[i] is already filled
+    if depth[i] != 0:
+        return
+
+    # If node at index i is root
+    if parent[i] == -1:
+        depth[i] = 1
+        return
+
+    # If depth of parent is not evaluated before,
+    # then evaluate depth of parent first
+    if depth[parent[i]] == 0:
+        fillDepth(parent, parent[i], depth)
+
+    # Depth of this node is depth of parent plus 1
+    depth[i] = depth[parent[i]] + 1
+
+
+# This function reutns height of binary tree represented
+# by parent array
+def findHeight(parent):
+    n = len(parent)
+    # Create an array to store depth of all nodes and
+    # initialize depth of every node as 0
+    # Depth of root is 1
+    depth = [0 for i in range(n)]
+
+    # fill depth of all nodes
+    for i in range(n):
+        fillDepth(parent, i, depth)
+
+    # The height of binary tree is maximum of all
+    # depths. Find the maximum in depth[] and assign
+    # it to ht
+    ht = depth[0]
+    for i in range(1, n):
+        ht = max(ht, depth[i])
+
+    return ht
+
 
 def main():
-  tree = TreeHeight()
-  tree.read()
-  print(tree.compute_height())
+    tree = TreeHeight()
+    tree.read()
+    # print(tree.compute_height_naive())
+    print(findHeight(tree.parent))
+
 
 threading.Thread(target=main).start()
